5277ad2e73f07a371e0052901a196e9e096ee7c5
Glauco Pater <>
2021-02-03 20:49:20 +0100

    Add world data

--- src/containers/EarthWithBoxes.js
+++ src/containers/EarthWithBoxes.js
@@ -2,31 +2,10 @@ import React, { Suspense, useRef, useState } from 'react'
 import { Canvas, useFrame, useLoader } from 'react-three-fiber'
 import { Controls } from '../components/Controls'
 import * as THREE from 'three'
-
+import { fetchLiveData, RADIUS_SPHERE, convertLatLon, latLongToVector3 } from "../utils"
+import data from "../data/countries_min.json"
 
 const makeUrl = (file) => `https://raw.githubusercontent.com/flowers1225/threejs-earth/master/src/img/${file}.jpg`
-
-
-const fixedPoints = [
-  [5, 3, 4],
-  [2, 6, 3],
-  [5, 4, 3],
-  [5, 1, 5],
-  [4, 3, 5],
-  [0, 5, 5],
-  [5, 5, -7],
-  [5, 5, -1],
-  [5, 5, 0],
-  [5, 5, 1],
-  [5, 5, 2],
-  [5, 5, 3],
-  [5, 5, 4],
-  [5, 5, 5],
-  [5, 5, 6],
-  [5, 5, 7]
-];
-
-
 
 export const Box = (props) => {
   // This reference will give us direct access to the mesh
@@ -66,60 +45,66 @@ export const Box = (props) => {
   )
 }
 
-/**
- *  (x - a)² + (y - b)² + (z - c)² = r²
- * 
- * 
- * x = r * cos(s) * sin(t)
- * y = r * sin(s) * sin(t)
- * z = r * cos(t)
- * 
- */
-const findPointsOnSphere = (d) => {
-  const points = [];
-  const minRange = 0;
-  const maxRange = 24;
-  for (var ss = minRange; ss < maxRange; ss++) {
-    for (var tt = 0; tt <= 12; tt++) {
-      const s = Math.PI * ss / 12;
-      const t = Math.PI * tt / 12;
-      var x = d * Math.cos(s) * Math.sin(t);
-      var y = d * Math.sin(s) * Math.sin(t);
-      var z = d * Math.cos(t);
-      points.push([x, y, z])
-    }
-  }
-  return points
-}
 
 const Earth = () => {
+
+  const [liveData, setLiveData] = React.useState([]);
+
+  React.useEffect(async () => {
+    const result = await fetchLiveData(liveData);
+    setLiveData(result);
+  }, [fetchLiveData])
+
+
   const [texture, bump] = useLoader(THREE.TextureLoader, [makeUrl('earth4'), makeUrl('earth_bump')])
   const ref = useRef()
+  const radium = RADIUS_SPHERE
 
   useFrame(() => {
     // ref.current.rotation.y += 0.001
   })
 
+  const points = data.map(([name, lat, long]) => {
+    const country = liveData.length > 0 && liveData.filter(item => item.location === name) || null
+    const population = country && country[0] && country[0].population || 0
+    const vaccinations = country && country[0] && country[0].vaccinations || 0
+    return {
+      name,
+      point: latLongToVector3(lat, long, 5, 2),
+      population: population,
+      vaccinations: vaccinations
+    }
+  })
 
-  const points = findPointsOnSphere(7);
-  console.log(points);
+  if (!liveData) {
+    console.log(liveData)
+    return <group ref={ref} name="loading"></group>
+  }
 
   return (
     <group ref={ref} name="earth">
-      {points.map((pos, index) => (
-        <mesh
-          key={index.toString()}
-          scale={[0.1, 0.1, 0.1]}
-          position={pos}
-          onClick={(e) => {
-            console.log(e.point.x, e.point.y, e.point.z)
-          }}>
-          <boxBufferGeometry args={[1, 1, 1]} />
-          <meshStandardMaterial color={'#ff0000'} />
-        </mesh>
-      ))}
-      <mesh visible position={[0, 0, 0]}>
-        <sphereBufferGeometry attach="geometry" args={[7, 64, 64]} />
+      {points.map(({ name, point, population, vaccinations }) => {
+        if (population > 0)
+          return <mesh
+            key={name}
+            scale={[0.1, 0.1, 0.1]}
+            position={point}
+            rotation={[45, 10, 0]}
+            onClick={(e) => {
+              console.log(name, population.toLocaleString(), vaccinations.toLocaleString(),
+                e.point.x, e.point.y, e.point.z, convertLatLon([e.point.x, e.point.y, e.point.z]))
+            }}>
+            <boxBufferGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color={'#ff0000'} />
+          </mesh>
+
+      })
+      }
+      <mesh
+        visible
+        position={[0, 0, 0]}
+      >
+        <sphereBufferGeometry attach="geometry" args={[radium, 64, 64]} />
         <meshStandardMaterial attach="material" map={texture} bumpMap={bump} bumpScale={0.05} />
       </mesh>
     </group>
